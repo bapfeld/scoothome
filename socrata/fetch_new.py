1de472b32c0ec3b00208e21bd49f31ca8cd5d469
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-17 23:09:39 -0600

    start to turn basic form into functions

--- socrata/fetch_new.py
+++ socrata/fetch_new.py
@@ -1,10 +1,50 @@
 import pandas as pd
 from sodapy import Socrata
-import configparser
+import configparser, argparse
+import os
+import psycopg2
 
-config = configparser.ConfigParser()
-config.read('/home/bapfeld/scoothome/socrata/socrata.ini')
-app_token = config['socrata']['app_token']
+def import_secrets(ini_path):
+    config = configparser.ConfigParser()
+    config.read(ini_path)
+    return (config['socrata']['app_token'], config['postgres'])
+    
 
-client = Socrata("data.austintexas.gov", app_token)
+def get_max_date():
+    pass
 
+def write_to_db(database, user, password, port):
+    pass
+
+def fetch_new_data(client, resource, start_date):
+    results = client.get(resource, where=f'Start Time > {latest_start_time}')
+    # results_df = pd.DataFrame.from_records(results)
+    return results
+    
+# '/home/bapfeld/scoothome/socrata/socrata.ini'
+# micromobility data: "7d8e-dm7r"
+
+def main():
+    def initialize_params():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            '--ini_path',
+            help="Path to the .ini file containing the app token",
+            required=False,
+        )
+        return parser.parse_args()
+    args = initialize_params()
+    app_token, pg = import_app_token(os.path.expanduser(args.ini_path))
+
+    # Establish a client to query the database
+    client = Socrata("data.austintexas.gov", app_token)
+
+    # Get latest dates for the different databases
+
+    # Fetch new data
+
+    # Write new data back to the database
+
+
+if __name__ == "__main__":
+    main()
