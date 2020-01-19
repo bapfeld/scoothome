5b84b774a5d6a77119ae095725548517b6caae84
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-18 19:22:09 -0600

    use variable

--- socrata/convert_time_series.sh
+++ socrata/convert_time_series.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROJ_ROOT=/home/bapfeld/scoothome
+
 python convert_to_ts.py \
-       --dat_path=~/scoothome/data/Shared_Micromobility_Vehicle_Trips.csv \
-       --dat_out=~/scoothome/data/time_series.csv
+       --dat_path=$PROJ_ROOT/data/Shared_Micromobility_Vehicle_Trips.csv \
+       --dat_out=$PROJ_ROOT/data/time_series.csv
