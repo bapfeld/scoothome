db4b90743528e13373b3f52aa22b7273fc083014
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-20 16:56:06 -0600

    switch to using a multiprocessing approach for the main file

--- socrata/convert_time_series.sh
+++ socrata/convert_time_series.sh
@@ -4,7 +4,6 @@ PROJ_ROOT=/home/bapfeld/scoothome
 
 python convert_to_ts.py \
        --dat_path=$PROJ_ROOT/data/Shared_Micromobility_Vehicle_Trips.csv \
-       --dat_out=$PROJ_ROOT/data/time_series.csv \
        --vehicle_type='scooter' \
-       --ini_path=$PROJ_ROOT/setup.ini \
-       --report=1000
+       --multi \
+       --multi_out='/home/bapfeld/scoothome/data/device_records/'
