29db1c31d714a4c1b92b7cea9fac75be05a41e50
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-20 23:05:18 -0600

    adds support for filling missing values with 0

--- socrata/combine_multi_ts_output.py
+++ socrata/combine_multi_ts_output.py
@@ -1,6 +1,52 @@
 import os, argparse
 from collections import Counter
+import numpy as np
 import pandas as pd
+
+def clean_df(df):
+    # Need to drop observations with essential missing data
+    df.dropna(inplace=True)
+
+    # And drop observations where the start/stop is out of bounds
+    df = df[df['Census Tract Start'] != 'OUT_OF_BOUNDS']
+    df = df[df['Census Tract End'] != 'OUT_OF_BOUNDS']
+
+    # Want to create a new variable to uniquely identify areas
+    df['location_start_id'] = df['Council District (Start)'].astype(str) + '-' + df['Census Tract Start']
+    df['location_end_id'] = df['Council District (End)'].astype(str) + '-' + df['Census Tract End']
+
+    # Convert time objects
+    df['start_time'] = pd.to_datetime(df['Start Time'], format="%m/%d/%Y %I:%M:%S %p")
+    df['end_time'] = pd.to_datetime(df['End Time'], format="%m/%d/%Y %I:%M:%S %p")
+    df['date'] = df['Start Time'].str.extract(r'(\d\d/\d\d/\d\d\d\d)?')
+
+    # Keep only the relevant information
+    drop_cols = ['Start Time',
+                 'End Time',
+                 'Modified Date',
+                 'Month',
+                 'Hour',
+                 'Day of Week',
+                 'Council District (Start)',
+                 'Council District (End)',
+                 'Year',
+                 'Census Tract Start',
+                 'Census Tract End']
+    df.drop(drop_cols, axis=1, inplace=True)
+    df.rename(columns={'ID': 'trip_id',
+                        'Device ID': 'device_id',
+                        'Vehicle Type': 'vehicle_type',
+                        'Trip Duration': 'duration',
+                        'Trip Distance': 'distance'},
+               inplace=True)
+
+    # Sort and reset the index
+    df.sort_values(['start_time'], inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    # Return the result
+    return df
+
 
 def main(dir_path, dat_path, calculate_missing, out_path):
     event_list = []
@@ -9,15 +55,39 @@ def main(dir_path, dat_path, calculate_missing, out_path):
     for f in f_list:
         with open(f, 'r') as f_in:
             event_list.extend(f_in.readlines())
-    if calculate_missing:
-        pass
-    else:
-        counts = pd.DataFrame.from_dict(Counter(event_list),
+
+    # Calculate the counts based on the full list
+    counts = pd.DataFrame.from_dict(Counter(event_list),
                                         orient='index',
                                         columns=['n']).reset_index()
-        counts['area'] = counts['index'].str.extract(r'^(.*?)--')
-        counts['time'] = counts['index'].str.extract(r'--(.*?)$')
+    counts['area'] = counts['index'].str.extract(r'^(.*?)--')
+    counts['time'] = counts['index'].str.extract(r'--(.*?)$')
 
+    # If we want to fill in missing place-times with 0s
+    if calculate_missing:
+        full_dat = pd.read_csv(os.path.expanduser(dat_path),
+                               dtype={'Census Tract Start': object,
+                                      'Census Tract End': object})
+        full_dat = clean_df(dat)
+        time_stamps = pd.date_range(full.dat.start_time.min(),
+                                    full.dat.end_time.max(),
+                                    freq="15min")
+        full_areas = set(list(pd.unique(full_dat['location_start_id'])) +
+                         list(pd.unique(full_dat['location_end_id'])))
+        full_ts = pd.DataFrame(np.zeros(len(time_stamps) * len(full_areas),
+                                        dtype=int),
+                               index=pd.MultiIndex.from_product([full_areas, time_stamps],
+                                                                names=['area', 'time']),
+                               columns=['n'])
+        full_ts['full_index'] = full_ts['area'] + '--' + full_ts['time'].astype(str)
+        places_exist = set(event_list)
+        full_places = set(tsm.ts['full_index'])
+        missing_places = full_places - places_exist
+        missing_df = pd.DataFrame.from_dict({k: 0 for k in missing_places},
+                                            orient='index',
+                                            columns=['n']).reset_index()
+        counts = pd.concat([counts, missing_df])
+    
     # write out again
     counts.to_csv(out_path,
                   columns=['area', 'time', 'n'],
