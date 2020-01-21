0191c30bb05e46c75fc41f59503444c20101184a
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-20 18:51:37 -0600

    adds skeleton for multipage pdf output of series

--- vis/time_vis.py
+++ vis/time_vis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 
 dat = pd.read_csv('/home/bapfeld/scoothome/data/nano_ts.csv')
@@ -10,3 +11,15 @@ dat.groupby('area')['n'].plot()
 fig.show()
 
 plt.close()
+
+# want to subset to only include non-zero areas?
+
+
+with PdfPages('/home/bapfeld/scoothome/figures/time_series_by_area.pdf') as pdf:
+    for i in range(areas):
+        subset = dat
+        plt.figure(figure=(11, 5))
+        subset.groupby('area')['n'].plot()
+        plt.title()
+        pdf.savefig()
+        plt.close()
