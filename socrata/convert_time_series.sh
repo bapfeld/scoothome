36135c299dcbdfc42c1dd57d7fc3f8af28b73ceb
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-21 15:53:54 -0600

    change path

--- socrata/convert_time_series.sh
+++ socrata/convert_time_series.sh
@@ -6,5 +6,5 @@ python convert_to_ts.py \
        --dat_path=$PROJ_ROOT/data/Shared_Micromobility_Vehicle_Trips.csv \
        --vehicle_type='scooter' \
        --multi \
-       --multi_out='/home/bapfeld/Dropbox/device_records/' \
+       --multi_out='/home/bapfeld/scoothome/data/device_records/' \
        --continue_process
