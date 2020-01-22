839f64ce495e9482dc760e21fddc823a3a25c912
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-21 21:36:36 -0600

    adds creation of weather table

--- socrata/create_db.sql
+++ socrata/create_db.sql
@@ -6,3 +6,15 @@ CREATE TABLE ts
 );
 
 COPY ts FROM '/home/postgres/empty_db.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE weather
+(
+  time TIMESTAMP,
+  temp REAL,
+  current_rain REAL,
+  rain_prob REAL,
+  humidity REAL,
+  wind REAL,
+  cloud_cover REAL,
+  uv INT
+);
