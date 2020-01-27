746b026c1bc37fb87e7ce76a8aff195649580542
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-27 09:21:37 -0600

    adds loop for all areas

--- src/prophet.py
+++ src/prophet.py
@@ -1,30 +1,61 @@
 import pandas as pd
 from fbprophet import Prophet
+import psycopg2
 from matplotlib.backends.backend_pdf import PdfPages
+from darksky.api import DarkSky
+from darksky.types import languages, units, weather
+import sys
+sys.path.append('/home/bapfeld/scoothome')
+from app.scoothome.model import tsModel, import_secrets
+import configparser
 
-# need to get the data in
-dat = pd.read_csv("/home/bapfeld/scoothome/data/jan_19_ts.csv")
 
-# let's select the most complete time series...
-area_list = list(dat.groupby('area')['n'].count().sort_values(ascending=False)[0:50].index)
+with open('/home/bapfeld/scoothome/data/area_id_list.txt', 'r') as f:
+    area_list = f.readlines()
+area_list = [x.strip() for x in area_list]
 
-# '3.0-48453000902'
+pg, ds_key = import_secrets('/home/bapfeld/scoothome/setup.ini')
 
-with PdfPages('/home/bapfeld/scoothome/figures/estimation_by_area_jan.pdf') as pdf:
-    for area in area_list:
-        test_dat = dat[dat['area'] == area].copy().reset_index(drop=True)
-        test_dat.drop(columns=['area'], inplace=True)
-        test_dat.rename(columns={'time': 'ds', 'n': 'y'}, inplace=True)
+# let's just fetch the weather forecast a single time...
+weather_only = tsModel(pg, ds_key)
+weather_only.get_weather_pred(lat = 30.267151, lon = -97.743057)
 
-        model = Prophet(changepoint_prior_scale=0.05)
-        model.fit(test_dat)
-        future = model.make_future_dataframe(periods=300, freq='H')
-        fcst = model.predict(future)
-        fig = model.plot(fcst)
-        # fig.show()
-        pdf.savefig()
 
-        fig2 = model.plot_components(fcst)
-        # fig2.show()
-        pdf.savefig()
+for area in area_list:
+    f_out = f'/home/bapfeld/scoothome/figures/ts/{area}.pdf'
+    m = tsModel(pg, ds_key)
+    m.get_area_series(area)
+    if m.area_series.shape[0] > 100:
+        m.get_weather_data()
+        m.prep_model_data()
+        m.build_model() # potential to change weather stuff here
+        m.train_model()
+        m.future_weather = weather_only.future_weather
+        m.build_prediction_df(lat = 30.267151, lon = -97.743057, periods=192, get_forecast=False)
+        m.future.dropna(inplace=True)
+        m.predict()
+        with PdfPages(f_out) as pdf:
+            fig = m.model.plot(m.fcst)
+            pdf.savefig()
+            fig2 = m.model.plot_components(m.fcst)
+            pdf.savefig()
+    
+
+# with PdfPages('/home/bapfeld/scoothome/figures/estimation_by_area_jan.pdf') as pdf:
+#     for area in area_list:
+#         test_dat = dat[dat['area'] == area].copy().reset_index(drop=True)
+#         test_dat.drop(columns=['area'], inplace=True)
+#         test_dat.rename(columns={'time': 'ds', 'n': 'y'}, inplace=True)
+
+#         model = Prophet(changepoint_prior_scale=0.05)
+#         model.fit(test_dat)
+#         future = model.make_future_dataframe(periods=300, freq='H')
+#         fcst = model.predict(future)
+#         fig = model.plot(fcst)
+#         # fig.show()
+#         pdf.savefig()
+
+#         fig2 = model.plot_components(fcst)
+#         # fig2.show()
+#         pdf.savefig()
 
