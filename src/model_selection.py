46dad35775f5a9baa72469e956d0f72604d9f23f
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-27 18:33:06 -0600

    adds some imports

--- src/model_selection.py
+++ src/model_selection.py
@@ -1,1 +1,19 @@
-test_area = '9.0-48453001100'
+import pandas as pd
+import numpy as np
+from fbprophet import Prophet
+import psycopg2
+from matplotlib.backends.backend_pdf import PdfPages
+from darksky.api import DarkSky
+from darksky.types import languages, units, weather
+import sys
+sys.path.append('/home/bapfeld/scoothome')
+from app.scoothome.model import tsModel, import_secrets
+import configparser
+
+
+def main():
+    test_area = '9.0-48453001100'
+
+if __name__ == "__main__":
+    main()
+
