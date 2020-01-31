500d61d97308f8347daa453620eb56264184c3cf
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-01-31 00:37:36 -0600

    uncomment original part of script

--- src/convert_time_series.sh
+++ src/convert_time_series.sh
@@ -2,12 +2,12 @@
 
 PROJ_ROOT=/home/bapfeld/scoothome
 
-# python convert_to_ts.py \
-    #        --dat_path=$PROJ_ROOT/data/Shared_Micromobility_Vehicle_Trips.csv \
-    #        --vehicle_type='scooter' \
-    #        --multi \
-    #        --multi_out='/home/bapfeld/scoothome/data/device_records/' \
-    #        --continue_process
+python convert_to_ts.py \
+       --dat_path=$PROJ_ROOT/data/Shared_Micromobility_Vehicle_Trips.csv \
+       --vehicle_type='scooter' \
+       --multi \
+       --multi_out='/home/bapfeld/scoothome/data/device_records/' \
+       --continue_process
 
 python convert_to_ts.py \
        --dat_path=$PROJ_ROOT/data/Shared_Micromobility_Vehicle_Trips.csv \
