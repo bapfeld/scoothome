69e097ff657b7f862a8e172b251e72331d8660a5
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-02 14:01:40 -0600

    import function from module

--- scoothomeflask/__init__.py
+++ scoothomeflask/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 from scoothomeflask import scoothome
+from scoothomeflask.scoothome.fetch_predictions import initialize_params
 args = initialize_params()
 pg, ds_key, map_pub_token = import_secrets(os.path.expanduser(args.ini_path))
