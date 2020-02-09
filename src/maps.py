dc633576d87866101ccc960f162f27fae9efbe0c
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-09 11:40:04 -0600

    Adds a shebang

--- src/maps.py
+++ src/maps.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import geopandas
 import matplotlib.pyplot as plt
 import contextily as ctx
@@ -7,16 +9,23 @@ import configparser
 import psycopg2
 
 def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.png'):
+    """
+    Function to add an underlay basemap to geopandas plot. 
+
+    Necessary if version of contextily does not contain function.
+    """
     xmin, xmax, ymin, ymax = ax.axis()
     basemap, extent = ctx.bounds2img(xmin, ymin, xmax, ymax, zoom=zoom, url=url)
     ax.imshow(basemap, extent=extent, interpolation='bilinear')
     # restore original x/y limits
     ax.axis((xmin, xmax, ymin, ymax))
 
+# Import postgres data
 config = configparser.ConfigParser()
 config.read('/home/bapfeld/scoothome/setup.ini')
 pg = config['postgres']
 
+# Query postgres db for summary of number of trips
 with psycopg2.connect(database=pg['database'],
                               user=pg['username'],
                               password=pg['password'],
@@ -25,8 +34,10 @@ with psycopg2.connect(database=pg['database'],
     query = 'SELECT area, COUNT(area) FROM ts WHERE N > 0 GROUP BY area'
     dat = pd.read_sql(query, conn)
 
+# Create a logged version of the variable
 dat['log_count'] = np.log(dat['count'])
 
+# Import the census and council district polygons
 tracts = geopandas.read_file('/home/bapfeld/scoothome/data/census_tracts')
 tracts = tracts[tracts['COUNTYFP'] == '453']
 tracts = tracts.to_crs({'init': 'epsg:4326'})
@@ -36,14 +47,7 @@ districts = geopandas.read_file('/home/bapfeld/scoothome/data/council_districts'
 austin = geopandas.sjoin(tracts, districts, op='intersects', how='inner')
 austin = austin.to_crs(epsg=3857)
 
-
-
-# ax = austin.plot(edgecolor='black', alpha=0.5, figsize=(10, 10))
-# add_basemap(ax, zoom=11, url=ctx.sources.ST_TONER_LITE) # 12 looks a little better but takes a long time
-# ax.set_axis_off()
-# plt.show()
-# plt.close()
-
+# Eliminate areas at the periphery
 sm_austin = austin[austin['geometry'].area < 12.0e+06].copy().reset_index(drop=True)
 sm_austin['centroid_real'] = sm_austin['geometry'].centroid
 lats = list(map(lambda val: val.x, sm_austin.centroid_real))
@@ -56,6 +60,7 @@ sm_austin['area'] = (sm_austin['council_di'].astype(str) + '-' + sm_austin['STAT
 sm_austin = sm_austin.merge(dat, on='area')
 
 # Building up some layers
+# Empty map of austin
 ax = sm_austin.plot(edgecolor='black', alpha=0.0, figsize=(8, 12))
 add_basemap(ax, zoom=12, url=ctx.sources.ST_WATERCOLOR)
 ax.set_axis_off()
@@ -65,6 +70,7 @@ plt.margins(0,0)
 plt.savefig('/home/bapfeld/scoothome/figures/austin_base_map.png')
 plt.close()
 
+# Map with outlines of areas
 ax = sm_austin.plot(edgecolor='black', facecolor='none', figsize=(8, 12))
 add_basemap(ax, zoom=12, url=ctx.sources.ST_WATERCOLOR)
 ax.set_axis_off()
@@ -74,7 +80,7 @@ plt.margins(0,0)
 plt.savefig('/home/bapfeld/scoothome/figures/austin_areas_map.png')
 plt.close()
 
-
+# Map with areas colored by frequency of trips
 fig, ax = plt.subplots(1, 1, figsize=(8, 12))
 sm_austin.plot(edgecolor='black', column='log_count', ax=ax)
 add_basemap(ax, zoom=12, url=ctx.sources.ST_WATERCOLOR)
