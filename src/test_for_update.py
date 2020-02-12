b1e765ee98660233b8620b8f1239a72ab508f89f
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-11 23:51:54 -0600

    adds small script to test if update needed

---
+++ src/test_for_update.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sodapy import Socrata
+import psycopg2, os, configparser
+import datetime
+
+# Get socrata and postgres secrts
+ini_path = os.path.expanduser('~/scoothome/setup.ini')
+def import_secrets(ini_path):
+    config = configparser.ConfigParser()
+    config.read(ini_path)
+    return (config['socrata']['app_token'],
+            config['postgres'])
+app_token, pg = import_secrets(ini_path)
+            
+# Determine the maximum date in the database
+with psycopg2.connect(database=pg['database'],
+                      user=pg['username'],
+                      password=pg['password'],
+                      port=pg['port'],
+                      host=pg['host']) as conn:
+    res = pd.read_sql_query('SELECT MAX(start_time) from rides', conn)
+current_max_date = res.iloc[0, 0]
+
+# Query socrata to see latest data
+client = Socrata("data.austintexas.gov", app_token)
+austin_res = client.get_metadata(dataset_identifier="7d8e-dm7r")
+austin_max = datetime.datetime.fromtimestamp(austin_res['rowsUpdatedAt'])
+austin_max_pretty = austin_max.strftime('%Y-%m-%d %H:%M')
+
+# Check to see the difference and determine action
+if (austin_max - current_max_date).days > 1:
+    action = 'update'
+else:
+    action = 'none'
+
+# Write the results to the update log file
+logfile = os.path.expanduser('~/update_check.log')
+log_note = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+log_note += f'\nDB MAX: {current_max_date} --- AUSTIN MAX: {austin_max_pretty}\n\n'
+log_note += f'ACTION: {action}\n'
+with open(logfile, 'a') as outfile:
+    outfile.writelines(log_note)
---
+++ src/test_for_update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ubuntu/.envs/scoothome/bin/python /home/ubuntu/scoothome/src/test_for_update.py
