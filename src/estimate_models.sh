97a488460532779f57ee8d2314db7dc02733a9e0
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-12 23:03:04 -0600

    change how multiple processes are implemented to support many cores

--- src/estimate_models.sh
+++ src/estimate_models.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-python estimate_models.py \
-       --ini_path=/home/bapfeld/scoothome/setup.ini \
-       --area_list_file=/home/bapfeld/scoothome/data/new_area_list.txt \
-       --vehicle_type='scooter' \
-       --completed_area_file=/home/bapfeld/scoothome/data/complete_model_list_1.txt \
-       --proc_num=1 \
-       --total_processes=3 &
-
-python estimate_models.py \
-       --ini_path=/home/bapfeld/scoothome/setup.ini \
-       --area_list_file=/home/bapfeld/scoothome/data/new_area_list.txt \
-       --vehicle_type='scooter' \
-       --completed_area_file=/home/bapfeld/scoothome/data/complete_model_list_2.txt \
-       --proc_num=2 \
-       --total_processes=3 &
-
-python estimate_models.py \
-       --ini_path=/home/bapfeld/scoothome/setup.ini \
-       --area_list_file=/home/bapfeld/scoothome/data/new_area_list.txt \
-       --vehicle_type='scooter' \
-       --completed_area_file=/home/bapfeld/scoothome/data/complete_model_list_3.txt \
-       --proc_num=3 \
-       --total_processes=3
-       
+for i in $(seq 1 $1)
+do
+    if [ $i != $1 ]
+    then
+        python estimate_models.py \
+               --ini_path=~/scoothome/setup.ini \
+               --area_list_file=~/scoothome/data/new_area_list.txt \
+               --vehicle_type='scooter' \
+               --completed_area_file=~/scoothome/data/complete_model_list_$i.txt \
+               --proc_num=$i \
+               --total_processes=$1 &
+    else
+        python estimate_models.py \
+               --ini_path=~/scoothome/setup.ini \
+               --area_list_file=~/scoothome/data/new_area_list.txt \
+               --vehicle_type='scooter' \
+               --completed_area_file=~/scoothome/data/complete_model_list_$i.txt \
+               --proc_num=$i \
+               --total_processes=$1
+    fi
+done
