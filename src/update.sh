677da2b75551b467ac87c579894d39daec83e4ae
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-13 23:32:38 -0600

    incorporates new model estimation

--- src/update.sh
+++ src/update.sh
@@ -7,5 +7,19 @@ git pull
 rm -rf /tmp/scooter_records/ /tmp/bicycle_records
 mkdir /tmp/scooter_records /tmp/bicycle_records
 
+# Fetch new data and convert to time series
 python update.py \
-       --ini_path=/home/bapfeld/scoothome/setup.ini
+       --ini_path=/home/bapfeld/scoothome/setup.ini \
+       --num_proc=$(nproc)
+
+# Calculate new scooter models
+python estimate_models.py \
+       --ini_path=~/scoothome/setup.ini \
+       --vehicle_type='scooter' \
+       --num_proc=$(nproc)
+
+# Calculate new bike models
+python estimate_models.py \
+       --ini_path=~/scoothome/setup.ini \
+       --vehicle_type='bicycle' \
+       --num_proc=$(nproc)
