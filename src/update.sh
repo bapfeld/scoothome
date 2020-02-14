7dc838ea869ce78efaa285943ec19d6a4ab9279b
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-13 23:29:13 -0600

    ensure directories exist and are clean

--- src/update.sh
+++ src/update.sh
@@ -3,5 +3,9 @@
 # make sure we have latest version of code
 git pull
 
+# make sure directory structure is clean for update
+rm -rf /tmp/scooter_records/ /tmp/bicycle_records
+mkdir /tmp/scooter_records /tmp/bicycle_records
+
 python update.py \
        --ini_path=/home/bapfeld/scoothome/setup.ini
