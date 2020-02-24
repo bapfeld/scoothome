8928427b2664f1f78ae401dcac1e4bd7bb2fb8c5
Brendan Apfeld <brendan.apfeld@utexas.edu>
2020-02-23 19:17:07 -0600

    converts to functions

--- src/test_for_update.py
+++ src/test_for_update.py
@@ -6,53 +6,59 @@ import boto3
 from botocore.exceptions import ClientError
 
 # Get socrata and postgres secrts
-ini_path = os.path.expanduser('~/scoothome/setup.ini')
 def import_secrets(ini_path):
     config = configparser.ConfigParser()
     config.read(ini_path)
     return (config['socrata']['app_token'],
             config['postgres'],
             config['ec2'])
-app_token, pg, ec2_config = import_secrets(ini_path)
             
 # Determine the maximum date in the database
-with psycopg2.connect(database=pg['database'],
-                      user=pg['username'],
-                      password=pg['password'],
-                      port=pg['port'],
-                      host=pg['host']) as conn:
-    res = pd.read_sql_query('SELECT MAX(start_time) from rides', conn)
-current_max_date = res.iloc[0, 0]
+def get_max_date(pg):
+    with psycopg2.connect(database=pg['database'],
+                          user=pg['username'],
+                          password=pg['password'],
+                          port=pg['port'],
+                          host=pg['host']) as conn:
+        res = pd.read_sql_query('SELECT MAX(start_time) from rides', conn)
+    current_max_date = res.iloc[0, 0]
+    return current_max_date
 
 # Query socrata to see latest data
-client = Socrata("data.austintexas.gov", app_token)
-austin_res = client.get_metadata(dataset_identifier="7d8e-dm7r")
-austin_max = datetime.datetime.fromtimestamp(austin_res['rowsUpdatedAt'])
-austin_max_pretty = austin_max.strftime('%Y-%m-%d %H:%M')
+def check_for_new_data(app_token):
+    client = Socrata("data.austintexas.gov", app_token)
+    austin_res = client.get_metadata(dataset_identifier="7d8e-dm7r")
+    austin_max = datetime.datetime.fromtimestamp(austin_res['rowsUpdatedAt'])
+    austin_max_pretty = austin_max.strftime('%Y-%m-%d %H:%M')
+    return (austin_max, austin_max_pretty)
 
 # Check to see the difference and determine action
-t_diff = (austin_max - current_max_date).days
-if t_diff > 1:
-    if t_diff > 6:
-        action = 'update'
+def calculate_action(austin_max, current_max_date):
+    t_diff = (austin_max - current_max_date).days
+    if t_diff > 1:
+        if t_diff > 6:
+            action = 'update'
+        else:
+            action = 'new data, no update'
     else:
-        action = 'new data, no update'
-else:
-    action = 'none'
+        action = 'none'
+    return action
 
 # Write the results to the update log file
-logfile = os.path.expanduser('~/update_check.log')
-log_note = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-log_note += f'\nDB MAX: {current_max_date} --- AUSTIN MAX: {austin_max_pretty}\n'
-log_note += f'ACTION: {action}\n\n'
-with open(logfile, 'a') as outfile:
-    outfile.writelines(log_note)
+def log_decision(action, current_max_date, austin_max_pretty):
+    logfile = os.path.expanduser('~/update_check.log')
+    log_note = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+    log_note += f'\nDB MAX: {current_max_date} --- AUSTIN MAX: {austin_max_pretty}\n'
+    log_note += f'ACTION: {action}\n\n'
+    with open(logfile, 'a') as outfile:
+        outfile.writelines(log_note)
 
 # If an update is required, start the updater instance and get the code running
-session = boto3.Session(profile_name='brendan-IAM')
-ec2 = session.client('ec2')
-response = ec2.describe_instances()
-updater_id = ec2_config['updater_id']
+def run_updater(ec2_config):
+    session = boto3.Session(profile_name='brendan-IAM')
+    ec2 = session.client('ec2')
+    response = ec2.describe_instances()
+    updater_id = ec2_config['updater_id']
 
 # This block tries in a dry run and raises an error only on failure
 try:
@@ -68,3 +74,15 @@ except ClientError as e:
         raise
 
 
+def main():
+    ini_path = os.path.expanduser('~/scoothome/setup.ini')
+    app_token, pg, ec2_config = import_secrets(ini_path)
+    current_max_date = get_max_date(pg)
+    austin_max, austin_max_pretty = check_for_new_data(app_token)
+    action = calculate_action(austin_max, current_max_date)
+    log_decision(action, current_max_date, austin_max_pretty)
+    if action == 'update':
+        run_updater(ec2_config)
+
+if __name__ == "__main__":
+    main()
